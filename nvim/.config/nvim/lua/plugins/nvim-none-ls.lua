fe1e3c66831ca2f15c6e50b174ed187c1907d18c
mr-mahmood <mr.manmahmood@gmail.com>
2025-12-23 21:13:55 +0330

    change some of codes

--- nvim/.config/nvim/lua/plugins/nvim-none-ls.lua
+++ nvim/.config/nvim/lua/plugins/nvim-none-ls.lua
@@ -1,18 +1,24 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.ast_grep,
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.prettier,
+				--null_ls.builtins.formatting.ast_grep,
 				null_ls.builtins.formatting.black.with({
 					extra_args = {
 						{ "--max-line-length", "500" },
 						{ "--ignore", "E501" },
 					},
 				}),
+				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.diagnostics.eslint"),
 			},
 		})
 		vim.keymap.set("n", "<leader>sf", vim.lsp.buf.format, {})
